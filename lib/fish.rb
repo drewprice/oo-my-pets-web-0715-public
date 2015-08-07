df28f0b874ae2f9e876a6c90c124c68675a8606b
Drew Price <drewprice@users.noreply.github.com>
2015-08-06 22:21:37 -0400

    Add solution

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,17 @@
 class Fish
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = 'nervous'
+  end
+
+  def make_happy
+    @mood = 'happy'
+  end
+
+  def get_nervous
+    @mood = 'nervous'
+  end
 end
